169420a928f36652bb2955271792371b7e62414b
Patchoco <nyankosan830@gmail.com>
2021-09-30 22:57:16 -0700

    Update SecondPlayerController.cs

--- Pong/Assets/SecondPlayerController.cs
+++ Pong/Assets/SecondPlayerController.cs
@@ -8,72 +8,36 @@ public class SecondPlayerController : MonoBehaviour
 
     public GameObject ball;
     public float moveSpeed = 20.0f;
-    bool isMoving = false;
-    float timeMoving = 0f;
-    Vector2 direction = Vector2.zero;
+    public float lerpSpeed = 1f;
+    private Rigidbody2D myRB;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enabled)
+        if (ball.transform.position.y > transform.position.y)
         {
-            float distance = Mathf.Abs(ball.transform.position.x - transform.position.x);
-
-            if (timeMoving > 0.1f)
+            if (myRB.velocity.y < 0)
             {
-                isMoving = false;
-                timeMoving = 0;
+                myRB.velocity = Vector2.zero;
+                myRB.velocity = Vector2.Lerp(myRB.velocity, Vector2.up * moveSpeed, lerpSpeed * Time.deltaTime);
             }
-
-            if (isMoving)
+            else if (ball.transform.position.y < transform.position.y)
             {
-                transform.Translate(direction * Time.deltaTime);
-                timeMoving += Time.deltaTime;
+                if (myRB.velocity.y > 0)
+                {
+                    myRB.velocity = Vector2.zero;
+                    myRB.velocity = Vector2.Lerp(myRB.velocity, Vector2.down * moveSpeed, lerpSpeed * Time.deltaTime);
+                }
             }
-
             else
             {
-                if (distance <= 15)
-                {
-                    if (ball.transform.position.z > transform.position.z + 2)
-                    {
-                        transform.Translate(new Vector2(0, 0) * -moveSpeed * Time.deltaTime);
-                        isMoving = true;
-                        direction = new Vector2(0, 0) * -moveSpeed;
-                    }
-
-                    if (ball.transform.position.z < transform.position.z - 2)
-                    {
-                        transform.Translate(new Vector3(0, 0) * moveSpeed * Time.deltaTime);
-                        isMoving = true;
-                        direction = new Vector2(0, 0) * moveSpeed;
-                    }
-                }
-
-                else
-                {
-                    float value = Random.Range(0f, 1f);
-                    
-                    if (value < 0.1)
-                    {
-                        transform.Translate(new Vector2(0, 0) * -moveSpeed * Time.deltaTime);
-                        isMoving = true;
-                        direction = new Vector2(0, 0) * -moveSpeed;
-                    }
-
-                    if (value < 0.2)
-                    {
-                        transform.Translate(new Vector3(0, 0) * moveSpeed * Time.deltaTime);
-                        isMoving = true;
-                        direction = new Vector2(0, 0) * moveSpeed;
-                    }
-                }
+                myRB.velocity = Vector2.Lerp(myRB.velocity, Vector2.zero * moveSpeed, lerpSpeed * Time.deltaTime);
             }
         }
     }
