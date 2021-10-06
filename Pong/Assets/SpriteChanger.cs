dc8e62b7921f6cd1f68744d804eddeb567ee6288
Patchoco <nyankosan830@gmail.com>
2021-10-06 15:38:21 -0700

    implementing game faces

---
+++ Pong/Assets/SpriteChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite Confident;
+    public Sprite Sad;
+    public Sprite Neutral;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer.sprite = Neutral;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void ConfidentFace()
+    {
+        spriteRenderer.sprite = Confident;
+
+        NeutralFace();
+    }
+
+    void SadFace()
+    {
+        spriteRenderer.sprite = Sad;
+        NeutralFace();
+    }
+
+    void NeutralFace()
+    {
+        spriteRenderer.sprite = Neutral;
+    }
+}
